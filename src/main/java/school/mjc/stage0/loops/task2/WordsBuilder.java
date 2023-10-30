5a51d48a9da11e2fee601faad6d4504ea47cd7a5
Abdulxafizx9 <abdulxafizx999@gmail.com>
2023-10-30 21:35:40 +0500

    done

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -1,7 +1,15 @@
 package school.mjc.stage0.loops.task2;
 
+import java.sql.SQLOutput;
+
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
+        for(int i = 0;i<chars.length;i++)
+        {
+            System.out.print(chars);
+        }
+
+
 
     }
 }
