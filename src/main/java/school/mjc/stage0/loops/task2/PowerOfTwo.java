f54737772a8db8ee66d43b18c4d5b0aa58a21a8a
Abdulxafizx9 <abdulxafizx999@gmail.com>
2023-10-31 20:59:03 +0500

    7.0

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -3,12 +3,13 @@ package school.mjc.stage0.loops.task2;
 import java.sql.SQLOutput;
 
 public class PowerOfTwo {
-    public void printPower(int power) {
+    public void printPower(int power)
+    {
         if(power>=0)
         {
             int i = 0;
             int result = 1;
-            while(i<power)
+            while(i<=power)
             {
                 if(i==0)
                     result=1;
